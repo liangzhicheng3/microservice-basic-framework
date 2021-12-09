9bc7b0038616ff2f9fc46be36e9611274b9ffa90
liangzhicheng <yichengc3@163.com>
2021-12-09 15:59:04 +0800

    refactor:优化用户登录后返回权限

--- user-server/src/main/java/com/liangzhicheng/modules/dao/ISysUserDao.java
+++ user-server/src/main/java/com/liangzhicheng/modules/dao/ISysUserDao.java
@@ -3,11 +3,15 @@ package com.liangzhicheng.modules.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.liangzhicheng.modules.entity.SysUserEntity;
 
+import java.util.List;
+
 /**
  * @description 账号 Mapper接口
  * @author liangzhicheng
  * @since 2021-08-09
  */
 public interface ISysUserDao extends BaseMapper<SysUserEntity> {
+
+    List<String> selectListByUserMenu(String accountId);
 
 }
